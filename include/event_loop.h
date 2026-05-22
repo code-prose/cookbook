c55a0d392bb261160ac2af0871305d9b06c7857c
Logan Rose <logprose@umich.edu>
2026-05-21 22:20:05 -0400

    fix: typing + move semantics

--- include/event_loop.h
+++ include/event_loop.h
@@ -3,14 +3,15 @@
 #include "types.h"
 #include <queue>
 #include <functional>
+#include <utility>
 
 
 class EventLoop {
 public:
-    void AddEvent(Event event) { _queue.push(event); };
-    void AddHandler(std::function<void(const Event&)> fn) { _handler = fn; };
+    void AddEvent(const Event& event) { _queue.push(event); };
+    void AddHandler(std::function<void(const Event&)> fn) { _handler = std::move(fn); };
 
-    // is it worth marking this noexcept?
+    // is it worth making this noexcept?
     void run();
 
 private:
