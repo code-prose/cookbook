38846405d8ae59e26e4e352ea5b3ac32ed6b9c64
Logan Rose <logprose@umich.edu>
2026-05-21 22:03:40 -0400

    feat: event loop header

---
+++ include/event_loop.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "types.h"
+#include <queue>
+#include <functional>
+
+
+class EventLoop {
+public:
+    void AddEvent(Event event);
+    void AddHandler(std::function<void(const Event&)> fn);
+    void run();
+
+private:
+    Event FetchEvent() const;
+
+    std::priority_queue<Event, std::vector<Event>, std::greater<Event>> _queue;
+    std::function<void(const Event&)> _handler;
+};
