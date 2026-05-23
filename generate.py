b65eb540aab61d7853f6dc15de80cc2e4abd6ee0
Logan Rose <logprose@umich.edu>
2026-05-22 23:57:12 -0400

    chore: py formatting

--- generate.py
+++ generate.py
@@ -1,16 +1,16 @@
 import polars as pl
 import numpy as np
 
+
 def main():
     # 0: timestamp, 1: id (instruments), 2: type, 3: trade_price, 4: trade_quantity, 5: side, 6: ask_price, 7: ask_quantity, 8: bid_price, 9: bid_quantity
     ROWS = 100
-    start = np.datetime64('2024-01-01', 'ns')
-    step = np.timedelta64(1_000_000, 'ns')
-    timestamps = [int((start + i * step).astype('int64')) for i in range(ROWS)]
+    start = np.datetime64("2024-01-01", "ns")
+    step = np.timedelta64(1_000_000, "ns")
+    timestamps = [int((start + i * step).astype("int64")) for i in range(ROWS)]
     instruments = ["TEST-ID" for _ in range(ROWS)]
     row_type = np.random.choice(["quote", "trade"], p=[0.7, 0.3], size=ROWS)
     sides = np.random.choice(["buy", "sell"], size=ROWS)
-
 
     price = [50000.0]
     for _ in range(1, ROWS):
@@ -22,36 +22,44 @@ def main():
         noise = np.random.randint(1, 100)
         quantity.append(quantity[-1] + noise)
 
-
-
     df = pl.DataFrame({"timestamp": timestamps}).with_columns(
         id=pl.lit(instruments),
         type=pl.lit(row_type),
         price=price,
         quantity=quantity,
-       trade_price=pl.when(pl.col("type") == "trade")
-                    .then(pl.col("price"))
-                    .otherwise(None),
-        trade_quantity=pl.when(pl.col("type") == "trade")
-                    .then(pl.col("quantity"))
-                    .otherwise(None),
+
+        trade_price=pl
+        .when(pl.col("type") == "trade")
+        .then(pl.col("price"))
+        .otherwise(None),
+
+        trade_quantity=pl
+        .when(pl.col("type") == "trade")
+        .then(pl.col("quantity"))
+        .otherwise(None),
 
         sides=sides,
-        side=pl.when(pl.col("type") == "trade")
-            .then(pl.col("sides"))
-            .otherwise(None),
-        ask_price=pl.when(pl.col("type") == "quote")
-                    .then(pl.col("price") + 0.5)
-                    .otherwise(None),
-        ask_quantity=pl.when(pl.col("type") == "quote")
-                    .then(pl.col("quantity"))
-                    .otherwise(None),
-        bid_price=pl.when(pl.col("type") == "quote")
-                    .then(pl.col("price") - 0.5)
-                    .otherwise(None),
-        bid_quantity=pl.when(pl.col("type") == "quote")
-                    .then(pl.col("quantity"))
-                    .otherwise(None),
+        side=pl.when(pl.col("type") == "trade").then(pl.col("sides")).otherwise(None),
+
+        ask_price=pl
+        .when(pl.col("type") == "quote")
+        .then(pl.col("price") + 0.5)
+        .otherwise(None),
+
+        ask_quantity=pl
+        .when(pl.col("type") == "quote")
+        .then(pl.col("quantity"))
+        .otherwise(None),
+
+        bid_price=pl
+        .when(pl.col("type") == "quote")
+        .then(pl.col("price") - 0.5)
+        .otherwise(None),
+
+        bid_quantity=pl
+        .when(pl.col("type") == "quote")
+        .then(pl.col("quantity"))
+        .otherwise(None),
     )
 
     df = df.drop(["sides", "quantity", "price"])
