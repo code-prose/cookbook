207cb1e5bb5beec2da3162088b25465e203c85f3
Logan Rose <logprose@umich.edu>
2026-05-23 00:14:25 -0400

    feat: test harness setup finished, want to write a strategy before perf testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <iostream>
+#include "event_loop.h"
+#include "data_feed.h"
+#include <print>
 
 
 int main(int argc, char * argv[]) {
-    std::cout << "Hello, world!" << std::endl;
+    std::string path = "./test_data.csv";
+
+    EventLoop event_loop{ };
+    DataFeed data_feed{path};
+    for (auto& event : data_feed) {
+        event_loop.AddEvent(event);
+        std::print("{}: {}\n", event.instrument, event.timestamp);
+    }
 }
