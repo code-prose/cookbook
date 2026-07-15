047d97760dee54dbfb55da3f09b1d15e1b8f907c
Logan Rose <logprose@umich.edu>
2026-07-14 21:09:55 -0400

    chore: I left myself a ton of optimization notes in the event parsing loop

--- src/data_feed.cpp
+++ src/data_feed.cpp
@@ -44,10 +44,18 @@ Event DataFeed::ParseEvent() {
     // is there a better way to parse this?
     // could I change the format?
     // is there a way to get this information without string comparison?
+    // what do I know about this data?
+    // I know that I always have the same number of commas
+    // I TECHNICALLY know that the ticker is always TEST-ID
+    // but this is not a good thing to work off of...
+    // I know that the first letter after the first comma is always a T or a Q
+    // what it be faster to do a SIMD pass over first to determine the type?
+    // ... does this even work? I would need a different # of something to determine
+    // I also don't love that std::variant has a vtable chase
     if (parts[2] == "trade") [[unlikely]] {
         // parsing logic
         int quant = std::stoi(parts[4]);
-        if (quant < 0) throw std::runtime_error("Quantity < 0");
+        if (quant < 0) throw std::runtime_error("Quantity < 0"); // kinda gross.. tom said to think about using less exceptions
         Quantity quantity{ static_cast<std::uint32_t>(quant)};
         Side side;
         if (parts[5] == "buy") {
@@ -57,19 +65,20 @@ Event DataFeed::ParseEvent() {
         }
 
         TradeEvent tradeEvent{ std::stof(parts[3]), side, quantity };
+        // no nrvo or rvo because I return based on branch
         return Event{ timestamp, instrument, tradeEvent };
-
-
     } else [[likely]] {
+        // is there a cleaner way to do this?
+        // this feels expensive
         int bQuant = std::stoi(parts[9]);
         int aQuant = std::stoi(parts[7]);
+        // am I guarding against an impossibility? I know I am for my generated events
         if (bQuant < 0) throw std::runtime_error("Bid quantity < 0");
         if (aQuant < 0) throw std::runtime_error("Ask quantity < 0");
         Quantity buyQuantity{ static_cast<std::uint32_t>(bQuant)};
         Quantity askQuantity{ static_cast<std::uint32_t>(aQuant)};
         QuoteEvent quoteEvent{ std::stof(parts[8]), buyQuantity, std::stof(parts[6]), askQuantity};
         return Event{ timestamp, instrument, quoteEvent};
-
     }
 
 }
