ee7b388749bb612307dae8f04adfd1aa3db1d429
Logan Rose <logprose@umich.edu>
2026-07-17 01:24:56 -0400

    feat: setting up mmap

--- include/data_feed.h
+++ include/data_feed.h
@@ -1,14 +1,30 @@
 #pragma once
-
 #include <fstream>
+#include <exception>
+
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
 #include "types.h"
+
 
 class DataFeed {
 public:
 
     DataFeed(const std::string& path) {
-        _fs = std::ifstream(path);
-        if (!_fs) throw std::runtime_error("Failed to open file: " + path);
+        // might need to specify some offset depending on how big the mapped file is
+
+        // test_data.csv is 169359815 bytes,  ~161mb... I don't think I can map something this large
+        auto fd = open(path.c_str(), O_RDONLY);
+        if (fd == -1) std::terminate();
+        struct stat file_stats{};
+        fstat(fd, &file_stats);
+
+        auto res = mmap(0, file_stats.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+        if (res == MAP_FAILED) std::terminate();
+        // _fs = std::ifstream(path);
+        // if (!_fs) throw std::runtime_error("Failed to open file: " + path);
 
         // discard headers
         std::getline(_fs, _line);
