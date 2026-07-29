ea41b96cf286031656977249f4615be6d1f4f820
Logan Rose <logprose@umich.edu>
2026-07-28 22:55:52 -0400

    chore: moving stuff over

--- include/mapped.h
+++ include/mapped.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <string>
+#include <exception>
+
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+struct MappedFile {
+    MappedFile(const std::string& path) {
+        auto fd = open(path.c_str(), O_RDONLY);
+        if (fd == -1) std::terminate();
+        struct stat file_stats{};
+        fstat(fd, &file_stats);
+
+        auto res = mmap(0, file_stats.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+        // for (auto itr = res; res != )
+
+        if (res == MAP_FAILED) std::terminate();
+    }
+
+    private:
+        void* mapped_{};
+};
