058aee0b67d161c8ae33c8595f53a1b478b12e25
Logan Rose <logprose@umich.edu>
2026-07-29 23:46:18 -0400

    feat: refactoring, comments about api decisions

--- include/mapped.h
+++ include/mapped.h
@@ -7,18 +7,29 @@
 #include <sys/stat.h>
 
 struct MappedFile {
-    MappedFile(const std::string& path) {
-        auto fd = open(path.c_str(), O_RDONLY);
-        if (fd == -1) std::terminate();
+    MappedFile(const std::string& path) : path_{path} {}
+    void open() {
+        fd_ = ::open(path_.c_str(), O_RDONLY);
+        if (fd_ == -1) std::terminate();
+
         struct stat file_stats{};
-        fstat(fd, &file_stats);
+        fstat(fd_, &file_stats);
+        size_ = file_stats.st_size;
+        // check result of fstat?
 
-        auto res = mmap(0, file_stats.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-        // for (auto itr = res; res != )
+        mapped_ = mmap(0, file_stats.st_size, PROT_READ, MAP_PRIVATE, fd_, 0);
+        if (mapped_ == MAP_FAILED) std::terminate();
+        
+        // need to strip csv headers... maybe this lives not in the struct
+    }
 
-        if (res == MAP_FAILED) std::terminate();
+    void close() {
+        munmap(mapped_, size_);
     }
 
     private:
+        int fd_{};
         void* mapped_{};
+        std::string path_{};
+        std::size_t size_{};
 };
