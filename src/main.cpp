b6d51e9de924c10105941eb44bde4c2fd45a3edf
Logan Rose <logprose@umich.edu>
2026-08-10 18:12:42 -0400

    chore: fixing setup for mmapped file

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "event_loop.h"
 #include "data_feed.h"
 #include "strategy.h"
-#include <print>
+#include "mapped.h"
 
 
 int main(int argc, char * argv[]) {
     std::string path = "./test_data.csv";
+    MappedFile mfile{path};
+    DataFeed data_feed{mfile};
 
     EventLoop event_loop{ };
-    DataFeed data_feed{path};
     OrderBook ob{ };
     NaiveStrategy ns{ob, 50'000.0f, 0.1f};
     event_loop.AddHandler([&ns](const Event& e) { ns.execute(e); });
