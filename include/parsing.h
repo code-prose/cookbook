0be3d1e2f098b04ded533dc8107b45ef87e5721d
Logan Rose <logprose@umich.edu>
2026-08-11 00:59:21 -0400

    feat: handrolled float parsing... division would need to go be done before going back to the market

--- include/parsing.h
+++ include/parsing.h
@@ -2,16 +2,22 @@
 #include <iterator>
 #include <cstdint>
 #include <cstring>
+#include <charconv>
 
 
 namespace CustomParsing {
    constexpr std::uint64_t powtable[] = {1, 10, 100, 1'000, 10'000};
    template <std::contiguous_iterator T>
    void int_from_float_chars(T begin, T end, std::uint64_t& val) {
-       auto decimal = std::memchr(begin, '.', static_cast<std::size_t>(begin - end));
-       std::uint64_t first_half = stoll(begin, decimal);
-       std::uint64_t second_half = stoll(decimal + 1, end);
-       first_half *= powtable[4 - static_cast<std::size_t>(end - decimal + 1)];
-       val = first_half + second_half;
+      auto decimal = static_cast<const char*>(std::memchr(begin, '.', static_cast<std::size_t>(end - begin)));
+
+      std::uint64_t whole{};
+      std::from_chars(begin, decimal, whole);
+
+      std::uint64_t frac{};
+      auto [ptr, ec] = std::from_chars(decimal + 1, end, frac);
+      auto frac_digits = static_cast<std::size_t>(ptr - (decimal + 1));
+
+      val = whole * powtable[4] + frac * powtable[4 - frac_digits];
    } 
 };
