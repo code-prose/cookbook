147f786d0858221a32412f7781bdc297c767ec87
Logan Rose <logprose@umich.edu>
2026-08-10 23:56:04 -0400

    fix: .data() on sv returns a raw ptr...

--- src/data_feed.cpp
+++ src/data_feed.cpp
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "data_feed.h"
 #include <chrono>
+#include <charconv>
 #include <stdexcept>
 
 const Event& DataFeed::Iterator::operator*() const {
@@ -31,9 +32,10 @@ Event DataFeed::ParseEvent() {
     std::vector<std::string_view> parts{MappedFile::split_sv(sv)};
 
     // 0: timestamp, 1: id, 2: type, 3: trade_price, 4: trade_quantity, 5: side, 6: ask_price, 7: ask_quantity, 8: bid_price, 9: bid_quantity can I use a better data format? can I mock a data stream?
-    std::int64_t since_epoch = std::stoll(parts[0].data());
+    std::int64_t since_epoch{};
+    std::from_chars(parts[0].begin(), parts[0].end(), since_epoch);
     Time timestamp{ std::chrono::nanoseconds(since_epoch) };
-    std::string instrument = parts[1].data();
+    std::string instrument{parts[1]};
 
     // getting fucked by branch prediction in multiple places
     // is there a better way to parse this?
@@ -47,7 +49,8 @@ Event DataFeed::ParseEvent() {
     // do these have the same number of elements? could I change the data format and then precompute and do a cmov?
     if (parts[2] == "trade") [[unlikely]] {
         // parsing logic
-        int quant = std::stoi(parts[4].data());
+        int quant{};
+        std::from_chars(parts[4].begin(), parts[4].end(), quant);
         if (quant < 0) throw std::runtime_error("Quantity < 0"); // kinda gross.. tom said to think about using less exceptions
         Quantity quantity{ static_cast<std::uint32_t>(quant)};
         Side side;
@@ -57,20 +60,28 @@ Event DataFeed::ParseEvent() {
             side = Side::Sell;
         }
 
-        TradeEvent tradeEvent{ std::stof(parts[3].data()), side, quantity };
+        float price{};
+        std::from_chars(parts[3].begin(), parts[3].end(), price);
+        TradeEvent tradeEvent{ price, side, quantity };
         // no nrvo or rvo because I return based on branch
         return Event{ timestamp, instrument, tradeEvent };
     } else [[likely]] {
         // is there a cleaner way to do this?
         // this feels expensive
-        int bQuant = std::stoi(parts[9].data());
-        int aQuant = std::stoi(parts[7].data());
+        int bQuant{};
+        int aQuant{};
+        std::from_chars(parts[9].begin(), parts[9].end(), bQuant);
+        std::from_chars(parts[7].begin(), parts[7].end(), aQuant);
         // am I guarding against an impossibility? I know I am for my generated events
         if (bQuant < 0) throw std::runtime_error("Bid quantity < 0");
         if (aQuant < 0) throw std::runtime_error("Ask quantity < 0");
         Quantity buyQuantity{ static_cast<std::uint32_t>(bQuant)};
         Quantity askQuantity{ static_cast<std::uint32_t>(aQuant)};
-        QuoteEvent quoteEvent{ std::stof(parts[8].data()), buyQuantity, std::stof(parts[6].data()), askQuantity};
+        float bidPrice{};
+        float askPrice{};
+        std::from_chars(parts[8].begin(), parts[8].end(), bidPrice);
+        std::from_chars(parts[6].begin(), parts[6].end(), askPrice);
+        QuoteEvent quoteEvent{ bidPrice, buyQuantity, askPrice, askQuantity};
         return Event{ timestamp, instrument, quoteEvent};
     }
 
