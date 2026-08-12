2953d7d5540fcb417488f2440106d57d2e78356a
Logan Rose <logprose@umich.edu>
2026-08-12 00:03:52 -0400

    chore: comments + cleanup

--- src/data_feed.cpp
+++ src/data_feed.cpp
@@ -23,16 +23,16 @@ bool DataFeed::Iterator::operator!=(const Iterator& other) const {
 }
 
 Event DataFeed::ParseEvent() {
+    // 0: timestamp, 1: id, 2: type, 3: trade_price, 4: trade_quantity, 5: side, 6: ask_price, 7: ask_quantity, 8: bid_price, 9: bid_quantity can I use a better data format? can I mock a data stream?
     std::string_view sv{};
     if (!mfile_.getline(sv)) {
         throw std::runtime_error("EOF");
         // can I do this without throwing?
     }
         
-    // if I don't allocate here then the lifetime is tied to mmap being open
     std::vector<std::string_view> parts{MappedFile::split_sv(sv)};
 
-    // 0: timestamp, 1: id, 2: type, 3: trade_price, 4: trade_quantity, 5: side, 6: ask_price, 7: ask_quantity, 8: bid_price, 9: bid_quantity can I use a better data format? can I mock a data stream?
+    // move to swar
     std::uint64_t since_epoch{0};
     auto ptr = parts[0].data();
     for (auto i{0uz}; i < 19; i++) {
@@ -41,16 +41,29 @@ Event DataFeed::ParseEvent() {
     Time timestamp{ std::chrono::nanoseconds(since_epoch) };
     std::string instrument{parts[1]};
 
-    // getting fucked by branch prediction in multiple places
-    // is there a better way to parse this?
-    // could I change the format?
-    // is there a way to get this information without string comparison?
-    // what do I know about this data?
-    // I know that I always have the same number of commas
-    // I know that the first letter after the first comma is always a T or a Q
-    // what it be faster to do a SIMD pass over first to determine the type?
-    // ... does this even work? I would need a different # of something to determine
-    // do these have the same number of elements? could I change the data format and then precompute and do a cmov?
+    // the branch predictor is actually super accurate here and precompute with cmov would just add unnecessary latency
+    // always check first...
+    // data:
+    // events: 2000000
+    // elapsed: 0.170286s
+    // ns/event: 85.1432
+    // checksum: 8198660791791185920
+    //
+    //  Performance counter stats for './build/microbench':
+    //
+    //             170.99 msec task-clock:u
+    //        736,144,717      cycles:u                                                                (83.00%)
+    //      2,632,964,826      instructions:u                                                          (83.04%)
+    //          4,926,044      cache-references:u                                                      (83.47%)
+    //             40,373      cache-misses:u                                                          (83.62%)
+    //        625,292,269      branches:u                                                              (83.65%)
+    //          1,589,856      branch-misses:u                                                         (83.21%)
+    //
+    //        0.171796613 seconds time elapsed
+    //
+    //        0.169223000 seconds user
+    //        0.000996000 seconds sys
+    //  1,589,856 / 625,292,269 ~= .25% misprediction
     if (parts[2] == "trade") [[unlikely]] {
         // parsing logic
         int quant{};
@@ -67,16 +80,14 @@ Event DataFeed::ParseEvent() {
         std::uint64_t price{};
         CustomParsing::int_from_float_chars(parts[3].begin(), parts[3].end(), price);
         TradeEvent tradeEvent{ price, side, quantity };
-        // no nrvo or rvo because I return based on branch
         return Event{ timestamp, std::move(instrument), tradeEvent };
     } else [[likely]] {
-        // is there a cleaner way to do this?
-        // this feels expensive
         int bQuant{};
         int aQuant{};
         std::from_chars(parts[9].begin(), parts[9].end(), bQuant);
         std::from_chars(parts[7].begin(), parts[7].end(), aQuant);
         // am I guarding against an impossibility? I know I am for my generated events
+        // I will almost never guess these wrong but maybe I should handle different than throwing an err
         if (bQuant < 0) throw std::runtime_error("Bid quantity < 0");
         if (aQuant < 0) throw std::runtime_error("Ask quantity < 0");
         Quantity buyQuantity{ static_cast<std::uint32_t>(bQuant)};
@@ -88,7 +99,6 @@ Event DataFeed::ParseEvent() {
         QuoteEvent quoteEvent{ bidPrice, buyQuantity, askPrice, askQuantity};
         return Event{ timestamp, std::move(instrument), quoteEvent};
     }
-
 }
 
 DataFeed::Iterator DataFeed::begin() {
@@ -96,7 +106,6 @@ DataFeed::Iterator DataFeed::begin() {
         Event first_event = ParseEvent();
         DataFeed::Iterator iter = {this, first_event, false};
         return iter;
-    // should I get rid of this?
     } catch (const std::runtime_error& e) {
         return end();
     }
